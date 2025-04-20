3f90ea01d0ecc9945139aa69ba6c85c9e728eda7
benbest96 <bestnzuzi@gmail.com>
2025-04-19 22:43:24 -0400

    aws_deployment

---
+++ innovative/dev/networking/variables.tf
@@ -0,0 +1,26 @@
+variable "region" {
+  default = "ca-central-1"
+}
+
+variable "vpc_cidr_block" {
+  default = "10.100.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  default = "10.100.10.0/24"
+}
+
+variable "private_subnet_cidr" {
+  default = "10.100.30.0/24"
+}
+variable "env" {
+  default = "dev"
+}
+
+variable "team" {
+  default = "BANK123"
+}
+
+variable "project" {
+  default = "WebAPP"
+}
